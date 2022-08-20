a5a91669b9d0c9eab30ef185515c65b2e68e32c1
DESKTOP-H39VBME\set14 <set140140@gmail.com>
2022-08-20 12:31:59 +0300

    Added installer

---
+++ AngryBirdsClone/Assets/Infrastructure/AngryInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Infrastructure
+{
+    public class AngryInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
